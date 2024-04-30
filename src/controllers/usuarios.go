e8e21f39811434ffc42152387dfbc25ab719c520
Marcio Jucelio <marciojucelio@gmail.com>
2024-04-30 16:05:59 -0300

    Configurando Rotas

---
+++ src/controllers/usuarios.go
@@ -0,0 +1,24 @@
+package controllers
+
+import "net/http"
+
+//CriarUsuario insert one user in database
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create User"))
+}
+//BuscarUsuarios searching all users in database
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Searching all users"))
+}
+//BuscarUsuario searching just on user in database
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Searching just one user"))
+}
+//AtualizarUsuario update a user from the database
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Update Usuario"))
+}
+//DeletarUsuario delete a user from the database
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Delete User"))
+}
---
+++ src/router/rotas/usuarios.go
@@ -0,0 +1,39 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasUsuarios = []Rota{
+	{
+		URI:                "/usuarios",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarUsuario,
+		RequerAutenticaçao: false,
+	},
+	{
+		URI:                "/usuarios",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuarios,
+		RequerAutenticaçao: false,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuario,
+		RequerAutenticaçao: false,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizarUsuario,
+		RequerAutenticaçao: false,
+	},
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarUsuario,
+		RequerAutenticaçao: false,
+	},
+}
