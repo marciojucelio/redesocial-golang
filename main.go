cb73bb493d08e317dc96d2d43e397beb6c023bb3
Marcio Jucelio <marciojucelio@gmail.com>
2024-05-01 23:48:32 -0300

    Abrindo conexao com o Banco de Dados

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config.Carregar()
 	r := router.Gerar()
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
---
+++ src/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+//Carregar vai inicializar as variáveis de ambiente
+var (
+	StringConexaoBanco = ""
+	Porta              = 0
+)
+
+func Carregar() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", 
+	os.Getenv("DB_USUARIO"), 
+	os.Getenv("DB_SENHA"),
+	os.Getenv("DB_NOME"),
+		)
+}
