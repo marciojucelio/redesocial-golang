58ef0fc24adce5c5213633e70945455d090e9bbc
Marcio Jucelio <marciojucelio@gmail.com>
2024-05-03 17:21:00 -0300

    Validação de Usuários

--- src/modelos/usuario.go
+++ src/modelos/usuario.go
@@ -1,7 +1,12 @@
 package modelos
 
-import "time"
-//Usuario representa um usuario usando a rede social
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+// Usuario representa um usuario usando a rede social
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
 	Nome     string    `json:"nome,omitempty"`
@@ -9,4 +14,33 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
+
+// Preparar vai chamar os metodos para validar e formatar o usuario recebido
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+	usuario.formatar()
+	return nil
+}
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("o campo Nome é obrigatório")
+	}
+	if usuario.Nick == "" {
+		return errors.New("o campo Nick é obrigatório")
+	}
+	if usuario.Email == "" {
+		return errors.New("o campo E-mail é obrigatório")
+	}
+	if usuario.Senha == "" {
+		return errors.New("o campo Senha é obrigatório")
+	}
+	return nil
+}
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
