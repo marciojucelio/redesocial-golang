1be8c8675e62e680c19c1b150e05d68bed88a951
Marcio Jucelio <marciojucelio@gmail.com>
2024-06-13 17:04:29 -0300

    Buscando Usuarios no Banco de Dados

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
 )
 
 // CriarUsuario insert one user in database
@@ -43,7 +44,21 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 
 // BuscarUsuarios searching all users in database
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Searching all users"))
+	nomeOuNick := strings.ToLower(r.URL.Query().Get("usuario"))
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarios, erro := repositorio.Buscar(nomeOuNick)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	}
+	respostas.JSON(w, http.StatusOK, usuarios)
 }
 
 // BuscarUsuario searching just on user in database
